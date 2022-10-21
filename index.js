58a8a716fb6c677efa6fd9e778c38c8c8442231c
Jose Angel Rodriguez Gonzalez <rodriguezgonzalez@uninorte.edu.co>
2022-10-21 14:54:02 -0500

    conexion database

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express()
+const mongoose = require('mongoose');
 
-const followersRoutes = require('./routes/followers')
+const timelineRoute = require('./routes/timeline')
 const tweetsRoutes = require('./routes/tweets')
 const usersRoutes = require('./routes/users')
 
+app.use(express.json());
+
+mongoose.connect(
+        "mongodb+srv://rootback:YPuJKnjzDoc52cDm@backtw.sll8h6g.mongodb.net/?retryWrites=true&w=majority"
+        )
+    .then(() => {
+        console.log("Exito");
+    })
+    .catch((e) => {
+        console.log(e)
+        console.log("Jumbo")
+    }) 
+
 app.use("/Usuarios", usersRoutes)
 app.use("/Publicaciones", tweetsRoutes)
-app.use("/Seguidores", followersRoutes)
+app.use("/Home", timelineRoute)
 
 
 app.listen(3000, () => {
