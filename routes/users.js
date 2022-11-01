35653296e1d0539ed772751dfdf730cdf9b880ef
Jose Angel Rodriguez Gonzalez <rodriguezgonzalez@uninorte.edu.co>
2022-10-31 21:36:27 -0500

    creacion enlace seguidor

--- routes/users.js
+++ routes/users.js
@@ -15,10 +15,7 @@ app.post('/', async (req,res) => {
 })
 
 app.put('/', async (req, res)=>{
-    const filter = {
-        name: req.body.name
-    }
-    const usuario = await Usuario.findOneAndUpdate(filter, req.body)
+    const usuario = await Usuario.findByIdAndUpdate(req.body.id, req.body)
     await usuario.save()
     res.status(200).json("Usuario actualizado")
 })
@@ -34,5 +31,47 @@ app.delete('/', async (req, res)=> {
 });
 })
 
+app.get('/:id/seguidores', async (req,res) => {
+
+    let seguidores = await Usuario.findById(req.params.id,{followers:1, _id: 0})
+    seguidores = seguidores.followers
+    res.status(200).json(seguidores)
+})
+
+
+app.get('/:id/seguidos', async (req,res) => {
+
+    let seguidos = await Usuario.findById(req.params.id,{following:1, _id: 0})
+    seguidos = seguidos.following
+    res.status(200).json(seguidos)
+})
+
+app.post('/seguir', async (req,res) => {
+    
+    let seguidos = await Usuario.findById(req.body.seguidor,{following: 1, _id: 0})
+    let seguidores = await Usuario.findById(req.body.seguido, {followers: 1, _id: 0})
+    seguidos = seguidos.following
+    seguidores = seguidores.followers
+    seguidos.push(req.body.seguido)
+    seguidores.push(req.body.seguidor)
+    const seguido = await Usuario.findByIdAndUpdate(req.body.seguido, {followers: seguidores})
+    seguido.save()
+    const seguidor = await Usuario.findByIdAndUpdate(req.body.seguidor, {following: seguidos})
+    seguidor.save()
+    res.status(200).json("Enlace seguido-seguidor creado exitosamente")
+})
+
+app.delete('/seguir',async (req,res) => {
+    
+    let seguidos = await Usuario.findById(req.body.seguidor,{following: 1, _id: 0})
+    let seguidores = await Usuario.findById(req.body.seguido, {followers: 1, _id: 0})
+    seguidos = seguidos.following.filter((e)=> e != req.body.seguido)
+    seguidores = seguidores.followers.filter((e)=> e != req.body.seguidor)
+    const seguido = await Usuario.findByIdAndUpdate(req.body.seguido, {followers: seguidores})
+    seguido.save()
+    const seguidor = await Usuario.findByIdAndUpdate(req.body.seguidor, {following: seguidos})
+    seguidor.save()
+    res.status(200).json("Enlace seguido-seguidor eliminado exitosamente")
+} )
 
 module.exports = app;
